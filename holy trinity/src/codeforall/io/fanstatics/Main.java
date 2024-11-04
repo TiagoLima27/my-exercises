7bd7e2b5d42f1a164107d0eab51b301041bb45bc
Tiago Lima <tiagolima27995@gmail.com>
2024-11-04 15:53:55 +0000

    Add files via upload
    { Exercise }
    map-filter-reduce holy trinity
    String message = "I'll send an SOS to the garbage world, " +
            "I hope that someone garbage gets my message in a garbage bottle.";
    Remove all garbage from the message and turn it to all caps

---
+++ holy trinity/src/codeforall/io/fanstatics/Main.java
@@ -0,0 +1,17 @@
+package codeforall.io.fanstatics;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "I'll send an SOS to the garbage world, " +
+                "I hope that someone garbage gets my message in a garbage bottle.";
+
+        String messageCaps = Arrays.stream(message.split(" "))
+                .filter(word -> !word.equals("garbage"))
+                .map(String::toUpperCase)
+                .reduce("", (a, b) -> a + " " + b);
+
+        System.out.println(messageCaps);
+    }
+}
